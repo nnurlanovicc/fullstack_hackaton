241ddc3122624faebbe442455b9dd55ad6c4ef48
nnurlanovicc <135005600+nnurlanovicc@users.noreply.github.com>
2023-09-18 16:13:57 +0600

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -35,7 +35,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/', schema_view.with_ui('swagger')),
-    path('api/v1/account/', include('account.urls')),
     path('api/v1/', include('profilee.urls')),
 ]
 
