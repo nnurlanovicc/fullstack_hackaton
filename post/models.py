8b3eb8f2f90e667f3dbd714cea7cab7a6cf8defc
nursultan <nursultankalilov1@gmail.com>
2023-09-18 13:22:46 +0600

    post1

--- post/models.py
+++ post/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+Reqruiter = get_user_model()
+
+
+
+class Post(models.Model):
+    reqruiter = models.ForeignKey(Reqruiter, on_delete=models.CASCADE, related_name='post')
+    company_name = models.CharField(max_length=30, blank=False)
+    vacancy = models.CharField(max_length=30, blank=False)
+    experience = models.PositiveIntegerField(max_length=2, blank=False)
+    salary = models.PositiveIntegerField(max_length=6, blank=False)
+    description = models.TextField(blank=False)
+    actuality = models.BooleanField(default=False)
+    created_ad = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
